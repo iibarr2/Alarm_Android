567c8fd5195acdcddcb702ef63e84c40f77ec761
Israel Ibarra <iibarr2@ilstu.edu>
2019-04-27 16:02:38 -0500

    Added functionality to alarm

--- app/src/main/java/com/example/alarm/AlarmFragment.java
+++ app/src/main/java/com/example/alarm/AlarmFragment.java
@@ -2,6 +2,8 @@ package com.example.alarm;
 
 import android.app.AlarmManager;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -9,7 +11,9 @@ import android.provider.AlarmClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,42 +25,16 @@ import android.widget.TextView;
 
 import java.util.Calendar;
 
-public class AlarmFragment extends Fragment implements View.OnClickListener {
+public class AlarmFragment extends DialogFragment {
 
     private static final String TAG = "AlarmFargment";
-    Button btnDate, btnTime, btnSetAlarm;
-    EditText txtDate, txtTime;
-    TextInputEditText message;
-    private int year, month, day, hour,minute;
-
-
-
-    @Nullable
+    @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.alarm_layout,container,false);
-        btnDate = (Button)view.findViewById(R.id.btn_date);
-        btnTime = (Button)view.findViewById(R.id.btn_time);
-        btnSetAlarm = (Button)view.findViewById(R.id.btn_setAlarm);
-        txtDate = (EditText)view.findViewById(R.id.in_date);
-        txtTime = (EditText)view.findViewById(R.id.in_time);
-        message = (TextInputEditText)view.findViewById(R.id.in_Message);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
 
-        btnDate.setOnClickListener(this);
-        btnTime.setOnClickListener(this);
-
-
-        return view;
-    }
-
-    @Override
-    public void onClick(View v) {
-        if(v == btnDate){
-            Calendar c = Calendar.getInstance();
-            year = c.get(Calendar.YEAR);
-            month = c.get(Calendar.MONTH);
-            day = c.get(Calendar.DAY_OF_MONTH);
-
-        }
+        return new TimePickerDialog(getActivity(), (TimePickerDialog.OnTimeSetListener) getActivity(), hour, minute, DateFormat.is24HourFormat(getActivity()));
     }
 }
