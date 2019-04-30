0b4de27146e80b6944629dae984d5677e2626c96
ryan busch <rwbusch@ilstu.edu>
2019-04-30 13:10:35 -0500

    timer unfinished but functional

--- app/src/main/java/com/example/alarm/MainActivity.java
+++ app/src/main/java/com/example/alarm/MainActivity.java
@@ -1,29 +1,11 @@
 package com.example.alarm;
 
-import android.app.AlarmManager;
-import android.app.Dialog;
-import android.app.PendingIntent;
-import android.app.TimePickerDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.provider.CalendarContract;
-import android.support.annotation.RequiresApi;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TimePicker;
 
-import java.text.DateFormat;
-import java.util.Calendar;
-
-public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
@@ -31,42 +13,22 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
 
     private ViewPager viewPager;
 
-    private EditText mEditText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
         viewPager = (ViewPager) findViewById(R.id.container);
         setupViewPager(viewPager);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_alarm);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_timer);
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_location);
-
-        mEditText = findViewById(R.id.in_time);
-
-        Button buttonTimePicker = findViewById(R.id.btn_selecttime);
-        buttonTimePicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DialogFragment timePicker = new DialogFragment();
-                timePicker.show(getSupportFragmentManager(), "time picker");
-            }
-        });
-
-        Button cancelButton = findViewById(R.id.btn_Cancel);
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cancelAlarm();
-            }
-        });
-
     }
 
     private void setupViewPager(ViewPager viewPager) {
@@ -75,45 +37,5 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
         adapter.addFragment(new TimerFragment());
         adapter.addFragment(new LocationFragment());
         viewPager.setAdapter(adapter);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
-        c.set(Calendar.MINUTE, minute);
-        c.set(Calendar.SECOND, 0);
-
-        updateTimeText(c);
-        startAlarm(c);
-    }
-
-    private void updateTimeText(Calendar c){
-        String timeText = DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
-        mEditText.setText(timeText);
-
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private void startAlarm(Calendar c){
-
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(this, AlertReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,1,intent,0);
-
-        if(c.before(Calendar.getInstance())){
-            c.add(Calendar.DATE, 1);
-        }
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(),pendingIntent);
-    }
-
-    private void cancelAlarm(){
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(this, AlertReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,1,intent,0);
-
-        alarmManager.cancel(pendingIntent);
-        mEditText.setText("Alarm Cancelled");
     }
 }
--- app/src/main/java/com/example/alarm/TimerFragment.java
+++ app/src/main/java/com/example/alarm/TimerFragment.java
@@ -1,6 +1,159 @@
 package com.example.alarm;
 
+import android.app.Dialog;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Timer;
 
 public class TimerFragment extends Fragment {
+
+    private static final String TAG = "TimerFragment";
+
+    private EditText mEditTextInput;
+    private TextView mTextViewCountDown;
+    private Button mButtonSet;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mStartTimeInMillis;
+    private long mTimeLeftInMillis;
+    private long mEndTime;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.timer_layout,container,false);
+
+
+        mEditTextInput = view.findViewById(R.id.edit_text_input);
+        mTextViewCountDown = view.findViewById(R.id.text_view_countdown);
+        mButtonSet = view.findViewById(R.id.button_set);
+        mButtonStartPause = view.findViewById(R.id.button_start_pause);
+        mButtonReset = view.findViewById(R.id.button_reset);
+
+        mButtonSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = mEditTextInput.getText().toString();
+                long millisInput = Long.parseLong(input) * 60000;
+                setTime(millisInput);
+                mEditTextInput.setText("");
+
+            }
+        });
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+        return view;
+
+    }
+
+    private void startTimer() {
+        mEndTime = System.currentTimeMillis() + mTimeLeftInMillis;
+
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                updateWatchInterface();
+            }
+        }.start();
+
+        mTimerRunning = true;
+        updateWatchInterface();
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        updateWatchInterface();
+    }
+
+    private void setTime(long milliseconds) {
+        mStartTimeInMillis = milliseconds;
+        resetTimer();
+    }
+    private void resetTimer() {
+        mTimeLeftInMillis = mStartTimeInMillis;
+        updateCountDownText();
+        updateWatchInterface();
+    }
+    private void updateCountDownText() {
+        int hours = (int) (mTimeLeftInMillis / 1000) / 3600;
+        int minutes = (int) ((mTimeLeftInMillis / 1000) % 3600) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted;
+        if (hours > 0) {
+            timeLeftFormatted = String.format(Locale.getDefault(),
+                    "%d:%02d:%02d", hours, minutes, seconds);
+        } else {
+            timeLeftFormatted = String.format(Locale.getDefault(),
+                    "%02d:%02d", minutes, seconds);
+        }
+
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+    private void updateWatchInterface() {
+        if (mTimerRunning) {
+            // mEditTextInput.setVisibility(View.INVISIBLE);
+            // mButtonSet.setVisibility(View.INVISIBLE);
+            // mButtonReset.setVisibility(View.INVISIBLE);
+            mButtonStartPause.setText("Pause");
+        } else {
+            // mEditTextInput.setVisibility(View.VISIBLE);
+            // mButtonSet.setVisibility(View.VISIBLE);
+            mButtonStartPause.setText("Start");
+
+
+        }
+    }
 }
+
+
+
+
